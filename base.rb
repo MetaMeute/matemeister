c1c34b83ad51dce82c6ab3e7c999e60da6a630ea
Anton Pirogov <anton.pirogov at gmail.com>
2012-12-07 23:49:49 +0100

    Almost finished

--- base.rb
+++ base.rb
@@ -4,15 +4,7 @@
 require 'sqlite3'
 
 DB_PATH = './data.db'
-
-#create empty database if not found
-if !File.exists?(DB_PATH)
-  SQLite3::Database.new(DB_PATH) do |db|
-    db.execute "CREATE TABLE users (name VARCHAR(32), matnr INTEGER, balance INTEGER);"
-    db.execute "CREATE TABLE drinks (name VARCHAR(32), stock INTEGER, price INTEGER);"
-    db.execute "CREATE TABLE logs (time INTEGER, entry VARCHAR(255));"
-  end
-end
+CURRENCY = ""
 
 class String
   def numeric?
@@ -23,17 +15,17 @@ end
 
 #interface to database
 class Base
-
   def update_cache
     @users = {}
+    @drinks = {}
     users.each{|r| @users[r[1]] = r[0]}
     @drinks = drinks.map{|a| a[0]}
   end
 
-  #make sure user exists, resolve matnr to username if given
+  #make sure user exists, resolve id to username if given
   #create new user if username does not exist
   #return username
-  #return nil if argument is invalid matnr
+  #return nil if argument is invalid id
   def touch_user(user)
     name = nil
     if user.class==String && user.numeric? || user.class==Fixnum
@@ -43,7 +35,8 @@ class Base
     end
 
     if name.nil?
-      return nil if user.class==String && user.numeric? #invalid matnr -> no creation, just fail
+      return nil if user.class==String && user.numeric? || user.class==Fixnum #invalid id -> no creation, just fail
+
       #create new user in system
       add_user(user)
       name = user
@@ -76,16 +69,24 @@ class Base
     @db.execute "SELECT * FROM users"
   end
 
-  def set_matnr(user,matnr=-1)
+  def set_id(user,id=-1)
     touch_user user
-    matnr = matnr.to_i if matnr.class == String && matnr.numeric?
-    @db.execute "UPDATE users SET matnr=#{matnr} WHERE name='#{user}'"
+    return false if user.nil?
+
+    id = id.to_i if id.class == String && id.numeric?
+
+    #no duplicate IDs
+    return false if id<=0 || users.map{|u| u[1]}.index(id)
+
+    @db.execute "UPDATE users SET id=#{id} WHERE name='#{user}'"
     update_cache
-    return nil
+    return true
   end
 
   def drink(user, num=1, drink=nil)
     user = touch_user user
+    return false if user.nil?
+
     drink = drinks[0][0] if drink.nil?
     drink = touch_drink drink
     num = num.to_i if num.class == String && num.numeric?
@@ -93,41 +94,55 @@ class Base
     v = get_balance user
     num.times{ v -= get_price drink }
     @db.execute "UPDATE users SET balance=#{v} WHERE name='#{user}'"
+    add_stock(drink,-num,false)
     log "#{user} trinkt #{num} Flasche(n) #{drink}."
-    return nil
+    return true
   end
 
   def pay(user,cent=0)
     user = touch_user user
+    return false if user.nil?
+
     cent = cent.to_i if cent.class == String && cent.numeric?
+    cent *= 100
 
     v = get_balance(user)+cent
     @db.execute "UPDATE users SET balance=#{v} WHERE name='#{user}'"
-    log "#{user} zahlt #{"%.2f" % (cent.to_f/100)}€ ein."
-    return nil
+    log "#{user} zahlt #{"%.2f" % (cent.to_f/100)}#{CURRENCY} ein."
+    return true
   end
 
   def transfer(from, to, cent=0)
     from = touch_user from
     to = touch_user to
+    return false if from.nil?
+    return false if to.nil?
+
+    return true if from == to
+
     cent = cent.to_i if cent.class == String && cent.numeric?
+    cent *= 100
 
     fbal = get_balance(from)-cent
     tbal = get_balance(to)+cent
     @db.execute "UPDATE users SET balance=#{fbal} WHERE name='#{from}'"
     @db.execute "UPDATE users SET balance=#{tbal} WHERE name='#{to}'"
-    log "#{from} überträgt #{"%.2f" % (cent.to_f/100)}€ auf #{to}."
-    return nil
+    log "#{from} überträgt #{"%.2f" % (cent.to_f/100)}#{CURRENCY} auf #{to}."
+    return true
   end
 
   def get_balance(user)
     user = touch_user user
+    return false if user.nil?
+
     @db.execute("SELECT balance FROM users WHERE name='#{user}'")[0][0]
   end
 
-  def get_matnr(user)
+  def get_id(user)
     user = touch_user user
-    @db.execute("SELECT matnr FROM users WHERE name='#{user}'")[0][0]
+    return false if user.nil?
+
+    @db.execute("SELECT id FROM users WHERE name='#{user}'")[0][0]
   end
 
   #log
@@ -138,7 +153,7 @@ class Base
 
   def log(str)
     @db.execute "INSERT INTO logs (time, entry) VALUES (#{Time.now.to_i}, '#{str}')"
-    return nil
+    return true
   end
 
   #drink functions
@@ -152,17 +167,17 @@ class Base
     drink = touch_drink drink
     cent = cent.to_i if cent.class == String && cent.numeric?
     @db.execute "UPDATE drinks SET price=#{cent} WHERE name='#{drink}'"
-    log "Getränk #{drink} kostet nun #{"%.2f" % (cent.to_f/100)}€!"
-    return nil
+    log "Getränk #{drink} kostet nun #{"%.2f" % (cent.to_f/100)}#{CURRENCY}!"
+    return true
   end
 
-  def add_stock(drink, num=0)
+  def add_stock(drink, num=0, l=true)
     drink = touch_drink drink
     num = num.to_i if num.class == String && num.numeric?
     oldstock = get_stock drink
     @db.execute "UPDATE drinks SET stock=#{oldstock+num} WHERE name='#{drink}'"
-    log "Getränk #{drink} mit #{num} Flasche(n) aufgestockt."
-    return nil
+    log "Getränk #{drink} mit #{num} Flasche(n) aufgestockt." if l
+    return true
   end
 
   def get_price(drink)
@@ -179,7 +194,7 @@ class Base
     @db.execute "DELETE FROM drinks WHERE name='#{drink}'"
     log("Getränk #{drink} entfernt!")
     update_cache
-    return nil
+    return true
   end
 
   #private
@@ -189,21 +204,36 @@ class Base
     @db.execute "DELETE FROM users WHERE name='#{user}'"
     log("Meuterer #{user} eliminiert!")
     update_cache
-    return nil
+    return true
   end
 
   def add_user(user)
-    @db.execute "INSERT INTO users (name, matnr, balance) VALUES ('#{user}', -1, 0)"
+    return false if users.map{|u| u[0]}.index(user)
+
+    @db.execute "INSERT INTO users (name, id, balance) VALUES ('#{user}', -1, 0)"
     log("Wilkommen, #{user}!")
     update_cache
-    return nil
+    return true
   end
 
   def add_drink(drink)
+    return false if drinks.map{|d| d[0]}.index(drink)
+
     @db.execute "INSERT INTO drinks (name, stock, price) VALUES ('#{drink}', 0, 100)"
     log("Neues Getränk #{drink} wurde eingeführt!")
     update_cache
-    return nil
+    return true
   end
-
 end
+
+#create empty database if not found
+if !File.exists?(DB_PATH)
+  SQLite3::Database.new(DB_PATH) do |db|
+    db.execute "CREATE TABLE users (name VARCHAR(32), id INTEGER, balance INTEGER);"
+    db.execute "CREATE TABLE drinks (name VARCHAR(32), stock INTEGER, price INTEGER);"
+    db.execute "CREATE TABLE logs (time INTEGER, entry VARCHAR(255));"
+    db.execute "INSERT INTO drinks (name, stock, price) VALUES ('mate', 0, 100)"
+  end
+end
+
+
--- matemeister.rb
+++ matemeister.rb
@@ -6,8 +6,7 @@
 require 'sinatra'
 require 'haml'
 
-#finish views
-#import data
+#TODO: add sum of drinks clicked by user for stats
 
 require './base'
 
@@ -34,10 +33,11 @@ end
 #direct database routes
 
 #user modification routes
-get '/user/:username/:action/:num/?:p?' do
+user_handler = lambda do
   u = params[:username]
   n = params[:num]
   return haml(:error) if u.match(/\w+/).to_s != u
+  return haml(:error) if u.empty?
   return haml(:error) if !n.numeric?
   n = n.to_i
 
@@ -45,20 +45,69 @@ get '/user/:username/:action/:num/?:p?' do
   p = params[:p]
 
   case a
-  when 'setmatnr'
-    $b.set_matnr(u,n)
-    haml :users
+  when 'setid'
+    if $b.set_id(u,n)
+      haml :users
+    else
+      haml :error
+    end
   when 'drink'
-    $b.drink(u,n,p)
-    haml :users
+    if $b.drink(u,n,p)
+      haml :users
+    else
+      haml :error
+    end
   when 'pay'
-    $b.pay(u,n)
-    haml :users
+    if $b.pay(u,n)
+      haml :users
+    else
+      haml :error
+    end
   when 'transfer'
     return haml(:error) if p.nil?
     return haml(:error) if p.match(/\w+/).to_s != p
-    $b.transfer(u,p,n)
-    haml :users
+    if $b.transfer(u,p,n)
+      haml :users
+    else
+      haml :error
+    end
+  else
+    haml :error
+  end
+
+end
+
+get '/user/:username/:action/:num/?:p?', &user_handler
+post '/', &user_handler
+
+drink_handler = lambda do
+  d = params[:drink]
+  a = params[:action]
+  p = params[:p]
+  return haml(:error) if d.match(/\w+/).to_s != d
+  return haml(:error) if d.empty?
+  return haml(:error) if !p.nil? && !p.numeric?
+  p = p.to_i
+
+  case a
+  when 'setprice'
+    if $b.set_price(d,p)
+      haml :drinks
+    else
+      haml :error
+    end
+  when 'addstock'
+    if $b.add_stock(d,p)
+      haml :drinks
+    else
+      haml :error
+    end
+  when 'rmdrink'
+    if $b.rm_drink(d)
+      haml :drinks
+    else
+      haml :error
+    end
   else
     haml :error
   end
@@ -66,11 +115,8 @@ get '/user/:username/:action/:num/?:p?' do
 end
 
 #drink modification routes
-get '/drink/:drink/:action/?:p?' do
-  d = params[:drink]
-  a = params[:action]
-  p = params[:p]
-end
+get '/drink/:drink/:action/?:p?', &drink_handler
+post '/drink', &drink_handler
 
 #close database on quitting
 trap(:INT){ $b.close }
